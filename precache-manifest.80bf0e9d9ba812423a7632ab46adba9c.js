8af948a124b42e62f33a15f692ecb075d1ccdbd8
Jason Bettyas <jbettyas@gmail.com>
2019-03-18 22:49:11 -0700

    Updates

---
+++ precache-manifest.80bf0e9d9ba812423a7632ab46adba9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8be7bc49c559bde2cf9",
+    "url": "/ColorGrid/static/css/main.c9c16d4f.chunk.css"
+  },
+  {
+    "revision": "c8be7bc49c559bde2cf9",
+    "url": "/ColorGrid/static/js/main.5ae57a38.chunk.js"
+  },
+  {
+    "revision": "68ef5abe0c0716f3437f",
+    "url": "/ColorGrid/static/js/runtime~main.8a738e7e.js"
+  },
+  {
+    "revision": "1bccc066d2189755bebb",
+    "url": "/ColorGrid/static/css/2.1b3c4fd9.chunk.css"
+  },
+  {
+    "revision": "1bccc066d2189755bebb",
+    "url": "/ColorGrid/static/js/2.b9f384f4.chunk.js"
+  },
+  {
+    "revision": "a1a28bd0f7986e066102e27556f635a2",
+    "url": "/ColorGrid/index.html"
+  }
+];
